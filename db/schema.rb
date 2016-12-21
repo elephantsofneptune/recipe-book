80bd8521bd3f4b08b73f6339cc6e3f2698077200
Kuan <pui_kuan93@hotmail.com>
2016-12-21 20:52:16 +0800

    added image for recipes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161221123320) do
+ActiveRecord::Schema.define(version: 20161221124826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20161221123320) do
     t.integer  "view_count"
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.string   "image"
     t.index ["cookbook_id"], name: "index_recipes_on_cookbook_id", using: :btree
   end
 
