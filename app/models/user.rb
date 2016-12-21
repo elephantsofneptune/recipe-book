ef36cc7c056c5d3151fed12afd36de2c7cefa487
Kuan <pui_kuan93@hotmail.com>
2016-12-21 19:11:14 +0800

    google omniauth done

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,18 @@ class User < ApplicationRecord
 
   enum access_level: [:cookbookers, :the_pretty_admin]
 
+def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      byebug
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = SecureRandom.urlsafe_base64
+      user.username = auth.info.email[/[^@]+/]
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.save!
+    end
+  end  
+
 end
