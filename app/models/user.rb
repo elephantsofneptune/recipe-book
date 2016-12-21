3a957e8a52f6f0d985bb0b3e52c4a9adbf664bdc
Kuan <pui_kuan93@hotmail.com>
2016-12-21 17:10:19 +0800

    added enum roles to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   has_many :cookbooks
   validates :username, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
   validates :email, :presence => true, :uniqueness => true
+
+  enum access_level: [:cookbookers, :the_pretty_admin]
+
 end
