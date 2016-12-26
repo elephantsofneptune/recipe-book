a00f13819249951a9f10a4f9cf62051dcb15be8e
Kuan <pui_kuan93@hotmail.com>
2016-12-27 01:29:31 +0800

    testing rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,43 +8,43 @@ RSpec.describe User, type: :model do
 
 	describe "email validation" do
 	  it "does not allow duplicate email to be signed up" do
-	  	expect(User.create!(email: "misokuan@gmail.com", username: "new_user", password: "1234Qwer")).to_be nil
+	  	expect{User.create!(email: "misokuan@gmail.com", username: "new_user", password: "1234Qwer")}.to raise_error(ActiveRecord::RecordInvalid)
 	  end
 
 	  it "allows unique email to be signed up" do
-	  	expect(User.create!(email: "pui_kuan93@hotmail.com", username: "new_user", password: "1234Qwer")).to_not_be nil
+	  	expect{User.create!(email: "pui_kuan93@hotmail.com", username: "new_user", password: "1234Qwer")}.to_not raise_error
 	  end 
 
 	  it "does not allow user without email to be signed up" do
-	  	expect(User.create!(username: "pui_kuan93", password: "1234Qwer")).to_be nil
+	  	expect{User.create!(username: "pui_kuan93", password: "1234Qwer")}.to raise_error(ActiveRecord::RecordInvalid)
 	  end
 	end  
 
 	describe "username validation" do
 	  it "does not allow duplicate username to be signed up" do
-	  	expect(User.create!(email: "new_user@gmail.com", username: "misokuan", password: "1234Qwer")).to_be nil
+	  	expect{User.create!(email: "new_user@gmail.com", username: "misokuan", password: "1234Qwer")}.to raise_error(ActiveRecord::RecordInvalid)
 	  end
 
 	  it "allows unique username to be signed up" do
-	  	expect(User.create!(email: "new_user@gmail.com", username: "pui_kuan93", password: "1234Qwer")).to_not_be nil
+	  	expect{User.create!(email: "new_user@gmail.com", username: "pui_kuan93", password: "1234Qwer")}.to_not raise_error
 	  end
 
 	  it "does not allow user without username to be signed up" do
-	  	expect(User.create!(email: "pui_kuan93@hotmail.com", password: "1234Qwer")).to_be nil
+	  	expect{User.create!(email: "pui_kuan93@hotmail.com", password: "1234Qwer")}.to raise_error(ActiveRecord::RecordInvalid)
 	  end	  
 	end
 
 	describe "#fullname" do
 	  it "returns the combination of first and last name" do
-	  	expect(@user_1.fullname).to_be "Miso Kuan"
-	  	expect(@user_2.fullname).to_be "Miss Npk"
+	  	expect(@user_1.full_name).to eq "Miso Kuan"
+	  	expect(@user_2.full_name).to eq "Miss Npk"
 	  end
 	end
 
 	describe '#age' do
 		it "returns the user's age based on their date of birth" do
-			expect(@user_1.age).to_be 23
-			expect(@user_2.age).to_be 12
+			expect(@user_1.age).to eq 23
+			expect(@user_2.age).to eq 12
 		end 
 	end	
 
