e19c1aeaca185a7e59998b6667d6a3ee5891160f
Kuan <pui_kuan93@hotmail.com>
2016-12-27 00:20:13 +0800

    fixing carrierwave issue;

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,6 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.autoload_paths += "#{Rails.root}/app/uploaders"
 
 module FinalAssessmentRecipebook
   class Application < Rails::Application
